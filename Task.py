41abc9c385de1e7b3f75b1801d0d13563a66055c
ValeriaBotalova <kaleriya.sych@mail.ru>
2025-02-22 09:08:45 +0300

    2 задание

--- Task.py
+++ Task.py
@@ -1,6 +1,6 @@
 import uuid
 
-class Figure():
+class Figure:
     def __init__(self, x=0, y=0):
         self._x = x
         self._y = y
@@ -13,16 +13,23 @@ class Figure():
         return self._x, self._y
     
     def move(self, dx, dy):
+        if not isinstance(dx, (int, float)) or not isinstance(dy, (int, float)):
+            raise ValueError("dx и dy должны быть числовыми значениями.")
+        
         self._x += dx
         self._y += dy
         print(f"Вы переместили фигуру {self.__id} на новую позицию: ({self._x}, {self._y}).")
 
     def __str__(self):
-        return f"Фигура с ID {self._id}"
+        return f"Фигура с ID {self.__id}"
 
 class Triangle(Figure):
     def __init__(self, side_a, height, x=0, y=0):
         super().__init__(x, y)
+        
+        if side_a <= 0 or height <= 0:
+            raise ValueError("Сторона и высота треугольника должны быть положительными.")
+        
         self._height = height
         self._side_a = side_a
 
@@ -32,6 +39,10 @@ class Triangle(Figure):
 class Rectangle(Figure):
     def __init__(self, side_a, side_b, x=0, y=0):
         super().__init__(x, y)
+        
+        if side_a <= 0 or side_b <= 0:
+            raise ValueError("Стороны прямоугольника должны быть положительными.")
+        
         self._side_a = side_a
         self._side_b = side_b
 
@@ -49,15 +60,21 @@ def compare(figure1: Figure, figure2: Figure) -> str:
     else:
         return f"Фигура {figure1.get_id()} равна по площади фигуре {figure2.get_id()}"
 
-triangle = Triangle(side_a=5, height=4)
-rectangle = Rectangle(side_a=4, side_b=5)
+try:
+    triangle = Triangle(side_a=4, height=4)
+    rectangle = Rectangle(side_a=4, side_b=5)
 
-print(triangle)
-print(rectangle)
-print(compare(triangle, rectangle))
+    print(triangle)
+    print(rectangle)
+    print(compare(triangle, rectangle))
 
-triangle.move(1, 2)
-rectangle.move(0, -1)
+    triangle.move(2, 2)
+    rectangle.move(0, -1)
+    
+    triangle.move(1, 2)
+    rectangle.move(5, 4)
 
-triangle.move(1, 2)
-rectangle.move(5, 4)
+except ValueError as ve:
+    print(f"Ошибка: {ve}")
+except Exception as e:
+    print(f"Произошла непредвиденная ошибка: {e}")
